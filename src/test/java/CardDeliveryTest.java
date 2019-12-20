0159ffc9918790cd6453f1e37d4449568b8a8d89
OlgaTyupina <57899824+OlgaTyupina@users.noreply.github.com>
2019-12-20 16:45:33 +0300

    Update CardDeliveryTest.java

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -1,11 +1,15 @@
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
 class CardDeliveryTest {
+    LocalDate date = LocalDate.now().plusDays(3);
+    
     @Test
     void shouldFormSubmission() {
         open("http://localhost:9999");
